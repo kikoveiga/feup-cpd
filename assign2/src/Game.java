d8127eab8097cf15b90ebac42cc84aa886186c02
antoniorama <antonio.rama.03@gmail.com>
2024-05-13 22:28:06 +0100

    Deal Cards function logic

--- assign2/src/Game.java
+++ assign2/src/Game.java
@@ -1,15 +1,45 @@
 import java.net.Socket;
+import java.util.LinkedList;
 import java.util.List;
 
+import game_logic.Deck;
+import game_logic.Card;
+
 public class Game {
-    private int players;
-    private List<Socket> userSockets;
-    public Game(int players, List<Socket> userSockets) {
-        this.userSockets = userSockets;
-        this.players = players;
+    private List<Client> playersList;
+    private Deck deck;
+    private List<Card> dealerHand;
+
+    public Game(int players, List<Client> playerSockets) {
+        this.playersList = playersList;
+        this.deck = new Deck();
+        this.dealerHand = new LinkedList<>();
     }
     public void start() {
-        // Code to start the game
-        System.out.println("Starting game with " + userSockets.size() + " players");
+        System.out.println("Starting blackjack game with " + playersList.size() + " players");
+    
+        // Deal initial two cards to each player and the dealer
+        for (Client client : playersList) {
+            dealInitialCards(client);
+        }
+
+        dealInitialCardsToDealer();
+    }
+
+    private void dealInitialCards(Client client) {
+        Card firstCard = deck.drawCard();
+        Card secondCard = deck.drawCard();
+
+        // TODO -> give cards to client
+    }
+
+    private void dealInitialCardsToDealer() {
+        Card firstCard = deck.drawCard();
+        Card secondCard = deck.drawCard();
+
+        this.dealerHand.add(firstCard);
+        this.dealerHand.add(secondCard);
+
+        // TODO -> handle this at the server
     }
 }
