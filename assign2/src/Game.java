c609f44511b53712b0641b95688dba5aea877b65
up202108777 <joaob3lchior@gmail.com>
2024-05-16 20:40:21 +0100

    basic game Class, parsing not working still

--- assign2/src/Game.java
+++ assign2/src/Game.java
@@ -1,35 +1,64 @@
+import java.io.File;
+import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 
-import game_logic.Deck;
-import game_logic.Card;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import game_logic.TriviaQuestion;
 
 public class Game {
     private List<Client> playerList;
-    private Deck deck;
-    private List<Card> dealerHand;
+    private List<TriviaQuestion> questions;
 
     public Game(List<Client> playerList) {
         this.playerList = playerList;
-        this.deck = new Deck();
-        this.dealerHand = new LinkedList<>();
+    }
+    // method to load questions into data set
+    public void loadQuestions(String dataPath){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String jsonData = new String(Files.readAllBytes(Paths.get(dataPath)));
+            JsonNode rootNode = objectMapper.readTree(jsonData);
+            JsonNode resultsNode = rootNode.path("results");
+
+            for (JsonNode questionNode : resultsNode) {
+                String question = questionNode.path("question").asText();
+                String correctAnswer = questionNode.path("correct_answer").asText();
+                TriviaQuestion triviaQuestion = new TriviaQuestion(question, correctAnswer);
+                questions.add(triviaQuestion);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void startGame(){
+
+        //test if questions work
+        for (TriviaQuestion q : questions){
+            System.out.println("question");
+            System.out.println(q.getQuestion());
+            System.out.println("answers");
+            System.out.println(q.getCorrectAnswer());
+        }
     }
 
-    private void dealInitialCards(Client client) {
-        Card firstCard = deck.drawCard();
-        Card secondCard = deck.drawCard();
-
-        // TODO -> give cards to client
+    // Broadcast a message to all players
+    public void broadcastMessage(String message) {
+        for (Client player : playerList) {
+            //player.sendMessage(message);
+        }
     }
 
-    private void dealInitialCardsToDealer() {
-        Card firstCard = deck.drawCard();
-        Card secondCard = deck.drawCard();
-
-        this.dealerHand.add(firstCard);
-        this.dealerHand.add(secondCard);
-
-        // TODO -> handle this at the server
+    public static void main(String[] args){
+        List<Client> players = new LinkedList<>();
+        Game game = new Game(players);
+        game.loadQuestions("/home/belchior/Desktop/g17/assign2/src/database/questions.json");
+        game.startGame();
     }
+
+
 }
